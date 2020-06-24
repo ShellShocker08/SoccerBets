55d7311ad1efa0b553902dba2f39929e294d43bb
Marco <mlunac08@outlook.com>
2020-06-24 12:14:49 -0500

    07 - Controller Explanation 1

--- Soccer.Web/Controllers/TeamsController.cs
+++ Soccer.Web/Controllers/TeamsController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data;
+using Soccer.Web.Data.Entities;
 
 namespace Soccer.Web.Controllers
 {
     public class TeamsController : Controller
     {
-        public IActionResult Index()
+        private readonly DataContext _context;
+
+        public TeamsController(DataContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Teams.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            TeamEntity teamEntity = await _context.Teams
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (teamEntity == null) return NotFound();
+
+            return View(teamEntity);
         }
     }
 }
