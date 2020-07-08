b178f46d67996f8ca1c70c1cc5386e9f27807547
Marco <mlunac08@outlook.com>
2020-07-07 19:56:03 -0500

    21 - Add API 1

---
+++ Soccer.Web/Controllers/API/TournamentsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data;
+using Soccer.Web.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TournamentsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TournamentsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTournaments()
+        {
+            List<TournamentEntity> tournaments = await _context.Tournaments
+                .Include(t => t.Groups)
+                .ToListAsync();
+            return Ok(tournaments);
+        }
+    }
+}
