348bfeecac42669131ac6effb49c2d84175a87ff
Marco <mlunac08@outlook.com>
2020-07-08 21:22:28 -0500

    25 - Show Tournaments On App 2

--- Soccer.Prism/Soccer.Prism/App.xaml.cs
+++ Soccer.Prism/Soccer.Prism/App.xaml.cs
@@ -1,8 +1,11 @@
 using Prism;
 using Prism.Ioc;
+using Soccer.Common.Interfaces;
+using Soccer.Common.Services;
 using Soccer.Prism.ViewModels;
-using Xamarin.Essentials.Interfaces;
+using Soccer.Prism.Views;
 using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,12 +13,7 @@ using Xamarin.Forms.Xaml;
 namespace Soccer.Prism
 {
     public partial class App
-    {
-        /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
+    {       
         public App() : this(null) { }
 
         public App(IPlatformInitializer initializer) : base(initializer) { }
@@ -24,14 +22,17 @@ namespace Soccer.Prism
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync("NavigationPage/TournamentPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+            // ApiService
+            containerRegistry.Register<IApiService, ApiService>();
 
-            containerRegistry.RegisterForNavigation<NavigationPage>();           
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<TournamentPage, TournamentPageViewModel>();
         }
     }
 }
---
+++ Soccer.Prism/Soccer.Prism/ViewModels/TournamentPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Soccer.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Soccer.Prism.ViewModels
+{
+    public class TournamentPageViewModel : ViewModelBase
+    {
+        public TournamentPageViewModel(INavigationService navigation)
+            : base(navigation)
+        {
+            Title = "Soccer";
+        }
+
+        public List<TournamentResponse> Tournaments { get; set; }
+    }
+}
