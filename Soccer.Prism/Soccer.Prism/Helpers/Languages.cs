740bd3a213c8526dfd0f5c624fa3edda1f77a8a3
Marco <mlunac08@outlook.com>
2020-07-13 10:54:08 -0500

    36 - Multilanguage in App 1

---
+++ Soccer.Prism/Soccer.Prism/Helpers/Languages.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Services;
+using Soccer.Common.Interfaces;
+using Soccer.Prism.Resources;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Soccer.Prism.Helpers
+{
+    public class Languages
+    {
+        static Languages()
+        {
+            CultureInfo ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            Culture = ci.Name;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Culture { get; set; }
+
+        public static string Accept => Resource.Accept;
+
+        public static string ConnectionError => Resource.ConnectionError;
+
+        public static string Error => Resource.Error;
+    }
+}
+
